0cf0204f85af5599389b2c832b2d5b6fee6d328d
Алина <@DESKTOP-306NSDC>
2021-06-03 11:23:54 +0300

    lab3 30

--- src/main/java/org/example/lab3/Packet.java
+++ src/main/java/org/example/lab3/Packet.java
@@ -1,5 +1,6 @@
-package org.example.lab3;
-import static org.example.lab3.CRC16.calculateCRC;
+package org.example.lab2;
+import static org.example.lab2.CRC16.calculateCRC;
+import static org.example.lab2.Network.MESSAGE_LENGTH;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -15,6 +16,8 @@ public class Packet {
     public final static Byte    B_MAGIC       = 0x13;
     public final static Integer HEADER_LENGTH = 16;
     public final static Integer CRC16_LENGTH = 2;
+
+
     public Message bMsq;
     public final byte bSrc;
     public final long bPktId;
@@ -24,6 +27,7 @@ public class Packet {
     public Packet(Byte bSrc, long bPktId, Message bMsq) {
         this.bSrc = bSrc;
         this.bPktId = bPktId;
+        this.cType = Message.cTypes.values()[bMsq.getCType()];
         this.bMsq = bMsq;
         wLen = bMsq.toBytes().length;
     }
@@ -37,16 +41,16 @@ public class Packet {
             throw new IllegalArgumentException("Unexpected bMagic");
 
         byte clientId = buffer.get();
-        System.out.println("Client ID: " + clientId);
+        //System.out.println("Client ID: " + clientId);
 
         long packetId = buffer.getLong();
-        System.out.println("Packet ID: " + packetId);
+        //System.out.println("Packet ID: " + packetId);
 
         int mLength = buffer.getInt();
-        System.out.println("Length:" + mLength);
+        //System.out.println("Length:" + mLength);
 
         short crcHead = buffer.getShort();
-        System.out.println("CRC16 of header:" + Integer.toBinaryString(0xFFFF & crcHead));
+       // System.out.println("CRC16 of header:" + Integer.toBinaryString(0xFFFF & crcHead));
 
         byte[] head = ByteBuffer.allocate(14)
                 .order(ByteOrder.BIG_ENDIAN)
@@ -67,6 +71,7 @@ public class Packet {
         ByteBuffer messageBuffer = ByteBuffer.wrap(decryptedBytes)
                 .order(ByteOrder.BIG_ENDIAN);
 
+        int cTypeId = messageBuffer.getInt();
         int bUserId = messageBuffer.getInt();
         byte[] messageBody = new byte[mLength - 8];
         messageBuffer.get(messageBody);
@@ -74,7 +79,7 @@ public class Packet {
 
         Message message = new Message(cType, bUserId, new String(messageBody));
 
-        message.setCType(buffer.getInt());
+        message.setCType(cTypeId);
         short crcMessage = buffer.getShort();
         if (calculateCRC(message.toBytes()) != crcMessage) {
             throw new IllegalArgumentException("Message CRC16");
@@ -83,11 +88,12 @@ public class Packet {
         return new Packet(clientId, packetId, message);
     }
 
+
     public byte[] toBytes() {
         byte[] messageBytes = bMsq.toBytes();
         byte[] encryptedBytes = Encryption.encrypt(messageBytes);
 
-        byte[] head = ByteBuffer.allocate(14)
+        byte[] head = ByteBuffer.allocate(MESSAGE_LENGTH)
                 .order(ByteOrder.BIG_ENDIAN)
                 .put(B_MAGIC)
                 .put(bSrc)
@@ -95,7 +101,8 @@ public class Packet {
                 .putInt(wLen)
                 .array();
 
-        return ByteBuffer.allocate(head.length + 2 + messageBytes.length + 2)
+        final Integer LENGTH =head.length + 2 + messageBytes.length + 2;
+        return ByteBuffer.allocate(LENGTH)
                 .order(ByteOrder.BIG_ENDIAN)
                 .put(head)
                 .putShort(calculateCRC(head))
